640a6d2528b705deb0f9b09fe4d54f62ee04f939
nitpengg <mda.ug22.ee@nitp.ac.in>
2025-07-04 12:43:37 +0530

    Project Completed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -30,7 +30,6 @@ export const getMessages = async (req, res) => {
 
     res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -64,7 +63,6 @@ export const sendMessage = async (req, res) => {
 
     res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
